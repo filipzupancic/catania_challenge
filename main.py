bf203945ee4bfaa117450144c7481124884f1c36
filipzupancic <filipzupancic123@gmail.com>
2018-08-02 13:37:57 +0200

    Adding messages

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import helper
-
-
+import messages
 
 print("test")
 user_id = helper.get_user_id_by_email(helper.USER1_MAIL)
@@ -9,7 +8,16 @@ print(user_id)
 chat = helper.get_chat(helper.USER1_MAIL, user_id)
 #print(chat)
 
-post_result1 = helper.post("kr en string", chat.id, None, user_id)  # johnny -> bot
+# parameters: user_id, screen_name, chat_id, user_id
+messages.comment_message("Ema", "Fucking for virginity", "https://www.intheloop.io/", "Poletna nebuloza", chat.id, user_id)
+
+# parameters: user_id, screen_name, chat_id, user_id
+messages.edit_message("Špela", "Bombing for peace", "https://www.intheloop.io/", chat.id, user_id)
+
+# parameters: user_id, screen_name, chat_id, user_id
+messages.create_message("Martina", "Drinking to end problems", "https://www.intheloop.io/", chat.id, user_id)
+
+#post_result1 = helper.post("kr en string", chat.id, None, user_id)  # johnny -> bot
 #print(post_result1)
-post_result2 = helper.post("še en string", chat.id, None, helper.BOT_ID)  # bot -> johnny
+#post_result2 = helper.post("še en string", chat.id, None, helper.BOT_ID)  # bot -> johnny
 #print(post_result2)
---
+++ messages.py
@@ -0,0 +1,34 @@
+import helper
+
+
+# file contains messages that are sent when new event occurs in application
+# and we want to notify a user
+
+# sends a notification about edit on project
+def edit_message(user_name, screen_name, project_link, chat_id, user_id):
+    if project_link is not None:
+        link = "Go to project: " + project_link
+    else:
+        link = ""
+    message = user_name + " made changes on screen #" + screen_name + "#.\n" + link
+    helper.post(message, chat_id, None, user_id)
+
+
+# sends a notification about new comment on project
+def comment_message(user_name, screen_name, project_link, comment, chat_id, user_id):
+    if project_link is not None:
+        link = "Go to project: " + project_link
+    else:
+        link = ""
+    message = user_name + " added new comment on screen #" + screen_name + "#. " + "\n\n@ " + comment + " @\n\n" + link
+    helper.post(message, chat_id, None, user_id)
+
+
+# sends a notification that new screen was created inside the project
+def create_message(user_name, screen_name, project_link, chat_id, user_id):
+    if project_link is not None:
+        link = "Go to project:\n" + project_link
+    else:
+        link = ""
+    message = user_name + " created new screen #" + screen_name + "#.\n" + link
+    helper.post(message, chat_id, None, user_id)
