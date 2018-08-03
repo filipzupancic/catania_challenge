ab7a9882ce1acc081e35d0950938f7f60f2372c2
filipzupancic <filipzupancic123@gmail.com>
2018-08-03 18:24:26 +0200

    Adding card object

---
+++ card.py
@@ -0,0 +1,21 @@
+class Card:
+
+    def __init__(self, marvel_token, project_pk, offset_comment):
+        self.marvel_token = marvel_token
+        self.project_pk = project_pk
+        self.offset_comment = offset_comment
+
+    def change_marvel_token(self, marvel_token):
+        self.marvel_token = marvel_token
+
+    def change_project_pk(self, project_pk):
+        self.project_pk = project_pk
+
+    def change_offset_comment(self, offset_comment):
+        self.offset_comment = offset_comment
+
+    def has_required_data(self):
+        if self.marvel_token is not None and self.project_pk is not None:
+            return True
+        else:
+            return False
