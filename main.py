6eb5def800cd714e4125746e6e78b8b7f0b6298a
mb5975 <mb5975@student.uni-lj.si>
2018-08-05 18:14:25 +0200

    Added message after sending update mail.

--- main.py
+++ main.py
@@ -136,9 +136,12 @@ while True:
                                 if type(resource) is User:
                                     mail_to_list.append(resource.email)
                             helper.send_mail("User", c_id, STATUS_MAIL_SUBJECT, data, mail_to_list)
+                            messages.send_message_to_chat_card("Done, check your inbox.", c_id, helper.BOT_ID)
                         else:
                             #send to group
                             helper.send_mail("Group", c_id, STATUS_MAIL_SUBJECT, data, mail_to_list)
+                            messages.send_message_to_chat_card("Done, check your inbox.", c_id, helper.BOT_ID)
+
 
                 elif messages.MAIL_UPDATE_WORD in comment.comment and not curr_card.has_required_data():
                     invalid_operation = False
